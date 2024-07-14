88a1885556b4732e5314e460bb13248bb5bd4843
pc <pc@DESKTOP-86M3HVC>
2024-07-15 01:02:50 +0300

    Mobile styles edited.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 
 
 <template>
-    <div v-if="this.$route.name == 'LOGIN' || this.$route.name == 'REGISTER' || this.$route.name == 'FORGOTPASSWORD' || this.$route.name == 'RESETPASSWORD'
-    || this.$route.name == 'BUSINESSREGISTER'">
+    <div v-if="!this.$store.getters._isAuthenticated">
         <router-view></router-view>
     </div>
     <div v-else class="layout-wrapper layout-content-navbar">
