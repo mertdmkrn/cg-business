c038c536c28394468a0ce0f6d96e3f726e11a273
ozcantolgahan <tolgahanozcan@hotmail.com>
2024-07-15 15:00:36 +0300

    Add some margin to special texts

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,10 +1,10 @@
 <script>
 import { initPasswordToggle } from '../../assets/js/helpers.js';
 export default {
-  mounted(){
+  mounted() {
     initPasswordToggle();
   },
-  data(){
+  data() {
     return {
       business: {
         email: "",
@@ -14,25 +14,23 @@ export default {
     }
   },
   methods: {
-    async onLogin(){
+    async onLogin() {
       await this.$appAxios.post("/business/login", this.business).then(response => {
-        var token = response?.data?.data?.token;             
-        if(token !== undefined)
-        {
+        var token = response?.data?.data?.token;
+        if (token !== undefined) {
           this.$store.commit("setToken", token);
           setTimeout(100);
-          this.$appAxios.post("/business/get", null, {headers: { 'Authorization': `Bearer ${token}` }}).then(businessResponse => {
+          this.$appAxios.post("/business/get", null, { headers: { 'Authorization': `Bearer ${token}` } }).then(businessResponse => {
             this.$store.commit("setBusiness", businessResponse?.data?.data),
-            this.$router.push({ path: '/' });
+              this.$router.push({ path: '/' });
           })
-          .catch(e => { console.log(e);  alert("Böyle bir kullanıcı bulunamadı.");});
+            .catch(e => { console.log(e); alert("Böyle bir kullanıcı bulunamadı."); });
         }
-        else
-        {
+        else {
           alert("Böyle bir kullanıcı bulunamadı.")
         }
       })
-      .catch(e => { console.log(e.message);  alert("Böyle bir kullanıcı bulunamadı.");});
+        .catch(e => { console.log(e.message); alert("Böyle bir kullanıcı bulunamadı."); });
     }
   },
 };
@@ -43,12 +41,8 @@ export default {
     <div class="authentication-inner row m-0">
       <div class="d-none d-lg-flex col-lg-8 p-0">
         <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
-          <img
-            src="../../assets/img/pages/auth-left.png"
-            alt="auth-login-cover"
-            data-app-light-img="pages/auth-left.png"
-            data-app-dark-img="pages/auth-left.png"
-          />
+          <img src="../../assets/img/pages/auth-left.png" alt="auth-login-cover" data-app-light-img="pages/auth-left.png"
+            data-app-dark-img="pages/auth-left.png" />
         </div>
       </div>
       <div class="d-flex col-12 col-lg-4 align-items-center p-sm-12 p-6">
@@ -59,29 +53,15 @@ export default {
           <form class="mb-6 fv-plugins-bootstrap5 fv-plugins-framework" @submit.prevent="onLogin">
             <div class="mb-6 fv-plugins-icon-container">
               <label for="email" class="form-label">{{ $t("Email") }}</label>
-              <input
-                type="text"
-                class="form-control"
-                id="email"
-                name="email"
-                v-model="business.email"
-                :placeholder="$t('EnterYourEmail')"
-                autofocus
-              />
+              <input type="text" class="form-control" id="email" name="email" v-model="business.email"
+                :placeholder="$t('EnterYourEmail')" autofocus />
               <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
             </div>
             <div class="mb-6 form-password-toggle fv-plugins-icon-container">
               <label class="form-label" for="password">{{ $t("Password") }}</label>
               <div class="input-group input-group-merge has-validation">
-                <input
-                  type="password"
-                  id="password"
-                  class="form-control"
-                  v-model="business.password"
-                  name="password"
-                  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                  aria-describedby="password"
-                />
+                <input type="password" id="password" class="form-control" v-model="business.password" name="password"
+                  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
                 <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
               </div>
               <div class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
@@ -89,12 +69,7 @@ export default {
             <div class="my-8">
               <div class="d-flex justify-content-between">
                 <div class="form-check mb-0 ms-2">
-                  <input
-                    class="form-check-input"
-                    v-model="business.isRememberMe"
-                    type="checkbox"
-                    id="remember-me"
-                  />
+                  <input class="form-check-input" v-model="business.isRememberMe" type="checkbox" id="remember-me" />
                   <label class="form-check-label" for="remember-me">{{ $t("RememberMe") }}</label>
                 </div>
                 <router-link :to="'/forgot-password'">
@@ -102,7 +77,8 @@ export default {
                 </router-link>
               </div>
             </div>
-            <button type="submit" class="btn btn-primary d-grid w-100 waves-effect waves-light" style="color: white;" :disabled="!(business.email && business.password)">
+            <button type="submit" class="btn btn-primary d-grid w-100 waves-effect waves-light" style="color: white;"
+              :disabled="!(business.email && business.password)">
               {{ $t("Login") }}
             </button>
           </form>
@@ -110,7 +86,7 @@ export default {
           <p class="text-center">
             <span>{{ $t("NewOnOurPlatform") }}</span>
             <router-link :to="'/register'">
-              <span>{{ $t("CreateAnAccount") }}</span>
+              <span style="margin-left: 4px;">{{ $t("CreateAnAccount") }}</span>
             </router-link>
           </p>
         </div>
@@ -120,5 +96,5 @@ export default {
 </template>
 
 <style scoped>
-  @import url('../../assets/vendor/css/pages/page-auth.css');
+@import url('../../assets/vendor/css/pages/page-auth.css');
 </style>
--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -1,22 +1,22 @@
 <script>
 import { initPasswordToggle } from '../../assets/js/helpers.js';
 export default {
-  mounted(){
+  mounted() {
     initPasswordToggle();
   },
-  data(){
-        return {
-            business: {
-                firstName: "",
-                lastName: "",
-                email: "",
-                telephone: "",
-                password: "",
-                rePassword: ""
-            },
-            isSave: false
-        }
-    },
+  data() {
+    return {
+      business: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        telephone: "",
+        password: "",
+        rePassword: ""
+      },
+      isSave: false
+    }
+  },
 }
 </script>
 
@@ -25,16 +25,11 @@ export default {
     <div class="authentication-inner row m-0">
       <div class="d-none d-lg-flex col-lg-8 p-0">
         <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
-          <img
-            src="../../assets/img/pages/auth-left.png"
-            alt="auth-login-cover"
-            class="auth-cover-bg"
-            data-app-light-img="pages/auth-left.png"
-            data-app-dark-img="pages/auth-left.png"
-          />
+          <img src="../../assets/img/pages/auth-left.png" alt="auth-login-cover" class="auth-cover-bg"
+            data-app-light-img="pages/auth-left.png" data-app-dark-img="pages/auth-left.png" />
         </div>
       </div>
-      
+
       <div v-if="!isSave" class="d-flex col-12 col-lg-4 align-items-center p-sm-12 p-6">
         <div class="w-px-400 mx-auto pt-5">
           <h4 class="mb-1">{{ $t("AdventureStartsHere") }}</h4>
@@ -43,76 +38,38 @@ export default {
           <form id="formAuthentication" class="mb-6">
             <div class="mb-6">
               <label for="firstname" class="form-label">{{ $t("FirstName") }}</label>
-              <input
-                v-model="business.firstName"
-                type="text"
-                class="form-control"
-                id="firstname"
-                name="firstname"
-                :placeholder="$t('EnterYourFirstName')"
-                autofocus
-              />
+              <input v-model="business.firstName" type="text" class="form-control" id="firstname" name="firstname"
+                :placeholder="$t('EnterYourFirstName')" autofocus />
             </div>
             <div class="mb-6">
               <label for="lastname" class="form-label">{{ $t("LastName") }}</label>
-              <input
-                v-model="business.lastName"
-                type="text"
-                class="form-control"
-                id="lastname"
-                name="lastname"
-                :placeholder="$t('EnterYourLastName')"
-              />
+              <input v-model="business.lastName" type="text" class="form-control" id="lastname" name="lastname"
+                :placeholder="$t('EnterYourLastName')" />
             </div>
             <div class="mb-6">
               <label for="email" class="form-label">{{ $t("Email") }}</label>
-              <input
-                v-model="business.email"
-                type="text"
-                class="form-control"
-                id="email"
-                name="email"
-                :placeholder="$t('EnterYourEmail')"
-              />
+              <input v-model="business.email" type="text" class="form-control" id="email" name="email"
+                :placeholder="$t('EnterYourEmail')" />
             </div>
             <div class="mb-6">
               <label for="telephone" class="form-label">{{ $t("MobileNumber") }}</label>
-              <input
-                v-model="business.telephone"
-                type="text"
-                class="form-control"
-                id="telephone"
-                name="telephone"
-                placeholder="05xx xxxx xx xx"
-              />
+              <input v-model="business.telephone" type="text" class="form-control" id="telephone" name="telephone"
+                placeholder="05xx xxxx xx xx" />
             </div>
             <div class="mb-6 form-password-toggle">
               <label class="form-label" for="password">{{ $t("Password") }}</label>
               <div class="input-group input-group-merge">
-                <input
-                  v-model="business.password"
-                  type="password"
-                  id="password"
-                  class="form-control"
-                  name="password"
-                  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                  aria-describedby="password"
-                />
+                <input v-model="business.password" type="password" id="password" class="form-control" name="password"
+                  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
                 <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
               </div>
             </div>
             <div class="mb-6 form-password-toggle">
               <label class="form-label" for="password">{{ $t("RePassword") }}</label>
               <div class="input-group input-group-merge">
-                <input
-                  v-model="business.rePassword"
-                  type="password"
-                  id="repassword"
-                  class="form-control"
-                  name="repassword"
-                  placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-                  aria-describedby="repassword"
-                />
+                <input v-model="business.rePassword" type="password" id="repassword" class="form-control"
+                  name="repassword" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                  aria-describedby="repassword" />
                 <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
               </div>
             </div>
@@ -131,50 +88,41 @@ export default {
           <p class="text-center">
             <span>{{ $t("AlreadyHaveAnAccount") }}</span>
             <router-link :to="'/login'">
-              <span>{{ $t("SignInInstead") }}</span>
+              <span style="margin-left: 4px;">{{ $t("SignInInstead") }}</span>
             </router-link>
           </p>
         </div>
       </div>
 
       <div v-else class="d-flex col-12 col-lg-4 align-items-center authentication-bg p-6 p-sm-12">
-          <div class="w-px-400 mx-auto mt-12 mt-5">
-            <h4 class="mb-1">{{ $t("TwoStepVerification") }}</h4>
-            <p class="text-start mb-6">
-                {{ $t("TwoStepVerificationSpot") }}
-              <span class="fw-medium d-block mt-1 text-heading">******1234</span>
-            </p>
-            <p class="mb-0">{{ $t("SecurityCodeText") }}</p>
-            <form id="twoStepsForm" action="index.html" method="GET">
-              <div class="mb-6">
-                <div class="auth-input-wrapper d-flex align-items-center justify-content-between numeral-mask-wrapper">
-                  <input
-                    type="tel"
-                    class="form-control auth-input h-px-50 text-center numeral-mask my-3"
-                    maxlength="1"
-                    autofocus />
-                  <input
-                    type="tel"
-                    class="form-control auth-input w-25 h-px-50 text-center numeral-mask my-3"
-                    maxlength="1" />
-                  <input
-                    type="tel"
-                    class="form-control auth-input w-25 h-px-50 text-center numeral-mask my-3"
-                    maxlength="1" />
-                  <input
-                    type="tel"
-                    class="form-control auth-input w-25 h-px-50 text-center numeral-mask my-3"
-                    maxlength="1" />
-                </div>
-                <input type="hidden" name="otp" />
+        <div class="w-px-400 mx-auto mt-12 mt-5">
+          <h4 class="mb-1">{{ $t("TwoStepVerification") }}</h4>
+          <p class="text-start mb-6">
+            {{ $t("TwoStepVerificationSpot") }}
+            <span class="fw-medium d-block mt-1 text-heading">******1234</span>
+          </p>
+          <p class="mb-0">{{ $t("SecurityCodeText") }}</p>
+          <form id="twoStepsForm" action="index.html" method="GET">
+            <div class="mb-6">
+              <div class="auth-input-wrapper d-flex align-items-center justify-content-between numeral-mask-wrapper">
+                <input type="tel" class="form-control auth-input h-px-50 text-center numeral-mask my-3" maxlength="1"
+                  autofocus />
+                <input type="tel" class="form-control auth-input w-25 h-px-50 text-center numeral-mask my-3"
+                  maxlength="1" />
+                <input type="tel" class="form-control auth-input w-25 h-px-50 text-center numeral-mask my-3"
+                  maxlength="1" />
+                <input type="tel" class="form-control auth-input w-25 h-px-50 text-center numeral-mask my-3"
+                  maxlength="1" />
               </div>
-              <button class="btn btn-primary d-grid w-100 mb-6">{{ $t("VerifyMyAccount") }}</button>
-              <div class="text-center">
-                {{ $t("DidntGetTheCode") }}
-                <a href="javascript:void(0);">{{ $t("Resend") }}</a>
-              </div>
-            </form>
-          </div>
+              <input type="hidden" name="otp" />
+            </div>
+            <button class="btn btn-primary d-grid w-100 mb-6">{{ $t("VerifyMyAccount") }}</button>
+            <div class="text-center">
+              {{ $t("DidntGetTheCode") }}
+              <a href="javascript:void(0);">{{ $t("Resend") }}</a>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -182,5 +130,5 @@ export default {
 
 
 <style scoped>
-  @import url('../../assets/vendor/css/pages/page-auth.css');
+@import url('../../assets/vendor/css/pages/page-auth.css');
 </style>
